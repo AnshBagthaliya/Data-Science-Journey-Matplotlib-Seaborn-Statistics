1f8fb3cb864fda9fe24e7e0e04f481244a713464
AnshBagthaliya <162408045+AnshBagthaliya@users.noreply.github.com>
2026-02-27 23:54:59 +0530

    Add files via upload

---
+++ 03_Matplotlib/introduction.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5]
+y = [10, 20, 25, 30, 40]
+
+# Create the plot
+plt.plot(x, y)
+
+# Add title and labels
+plt.title("Simple Line Graph")
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+
+# Show the plot
+plt.show()
